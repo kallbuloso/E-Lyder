cd8bc6decfe66445f0d60e60346ea853395dc2e1
kallbuloso <kallbuloso@gmail.com>
2018-10-04 21:34:49 -0300

    Novo Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,63 +1,94 @@
 @extends('layouts.auth.app')
 
 @section('content')
-    {{-- <form class="form-horizontal" method="POST" action="{{ route('login') }}"> --}}
-        @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
+<div class="card-header border-0">
+    <div class="card-title text-center">
+      <img src="ma/app-assets/images/logo/logo-dark.png" alt="branding logo">
+    </div>
+    <h6 class="card-subtitle line-on-side text-muted text-center font-small-3 pt-2">
+      <span>Easily Using</span>
+    </h6>
+    </div>
+    <div class="card-content">
+        <div class="text-center">
+        <a href="#" class="btn btn-social-icon mr-1 mb-1 btn-outline-facebook">
+            <span class="la la-facebook"></span>
+        </a>
+        <a href="#" class="btn btn-social-icon mr-1 mb-1 btn-outline-twitter">
+            <span class="la la-twitter"></span>
+        </a>
+        <a href="#" class="btn btn-social-icon mr-1 mb-1 btn-outline-linkedin">
+            <span class="la la-linkedin font-medium-4"></span>
+        </a>
+        <a href="#" class="btn btn-social-icon mr-1 mb-1 btn-outline-github">
+            <span class="la la-github font-medium-4"></span>
+        </a>
         </div>
-        @endif
-        @if (session('warning'))
-            <div class="alert alert-warning">
-                {{ session('warning') }}
+        <p class="card-subtitle line-on-side text-muted text-center font-small-3 mx-2 my-1">
+        <span>Fazer Login</span>
+        </p>
+        <div class="card-body">
+            {{--  Status Session  --}}
+            @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
             </div>
-        @endif
-        {!! Form::open(['class' => "form-horizontal", 'method' => 'POST']) !!}
-            {{-- {{ csrf_field() }} --}}
-            <h1>Fazer Login</h1>
-            <div class="{{ $errors->has('email') ? ' has-error' : '' }}">
-                <input id="email" type="email" class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="Seu e-mail" name="email" value="{{ old('email') }}" required autofocus>
-                {{--  error  --}}
-                @if ($errors->has('email'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                @endif
-            </div>
-
-            {{--  input password  --}}
-            <div class="{{ $errors->has('email') ? ' has-error' : '' }}">
-                <input type="password" class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Password') }}" name="password" required>
-                {{--  error  --}}
-                @if ($errors->has('password'))
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                @endif
-            </div>
-           
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                <label class="form-check-label" for="remember">
-                    {{ __('Permanecer logado') }}
-                </label>
-            </div>
-
-            <div class="form-group">
-                <button type="submit"  class="btn btn-primary" >Entrar</button>
-                <a class="forgot-password padding-top-10" href="{{ route('password.request') }}">Esqueci a senha</a>
-            </div>
-            <div class="clearfix"></div>
-            <div class="separator">
-                <p class="change_link">Novo no site?
-                    <a href="{{ route('register') }}" class="to_register"> Crie uma nova conta </a>
-                </p>
-
-                <div class="clearfix"></div>
-                <br /> 
-                @include('layouts.footer')
-            </div>
-        {!! Form::close() !!}
-    
+            @endif
+            @if (session('warning'))
+                <div class="alert alert-warning">
+                    {{ session('warning') }}
+                </div>
+            @endif
+            {!! Form::open(['class' => "form-horizontal", 'method' => 'POST']) !!}
+                {{--  E-mail  --}}
+                <fieldset class="form-group position-relative has-icon-left {{ $errors->has('email') ? ' has-error' : '' }}">
+                    <input id="email" type="email" class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Seu email') }}" name="email" value="{{ old('email') }}" required autofocus>
+                    <div class="form-control-position">
+                        <i class="ft-user"></i>
+                    </div>
+                    {{--  error  --}}
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </fieldset>
+                {{--  Senha  --}}
+                <fieldset class="form-group position-relative has-icon-left {{ $errors->has('password') ? ' has-error' : '' }}">
+                        <input type="password" class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Password') }}" name="password" required>
+                    <div class="form-control-position">
+                        <i class="la la-key"></i>
+                    </div>
+                    {{--  error  --}}
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </fieldset>
+                <div class="form-group row">
+                    <div class="col-md-6 col-12 text-center text-sm-left">
+                        <fieldset>
+                            <input type="checkbox" id="remember-me" class="chk-remember" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <label for="remember-me">{{ __('Permanecer logado') }}</label>
+                        </fieldset>
+                    </div>
+                    <div class="col-md-6 col-12 float-sm-left text-center text-sm-right">
+                        <a href="{{ route('password.request') }}" class="card-link">{{ __('Esqueci a senha') }}</a>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-outline-info btn-block"><i class="ft-unlock"></i> {{ __('Entrar agora') }}</button>
+            {!! Form::close() !!}
+        </div>
+        <p class="card-subtitle line-on-side text-muted text-center font-small-3 mx-2 my-1">
+            <span>{{ __('Novo no site?') }}</span>
+        </p>
+        <div class="card-body">
+            <a href="{{ route('register') }}" class="btn btn-outline-danger btn-block">
+                <i class="ft-user"></i> 
+                {{ __(' Crie uma conta ') }}
+            </a>
+        </div>
+    </div>    
 @endsection
+
--- resources/views/layouts/auth/app.blade.php
+++ resources/views/layouts/auth/app.blade.php
@@ -1,39 +1,68 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="loading"  lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-textdirection="ltr">
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-		<!-- Bootstrap -->
-		<link href="{{ asset('admin-assets/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
-		<!-- Font Awesome -->
-		<link href="{{ asset('admin-assets/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
-		<!-- NProgress -->
-		<link href="{{ asset('admin-assets/vendors/nprogress/nprogress.css') }}" rel="stylesheet">
-    <!-- Animate.css -->
-    <link href="{{ asset('admin-assets/vendors/animate.css/animate.min.css') }}" rel="stylesheet">
-
-    <!-- Custom Theme Style -->
-    <link href="{{ asset('admin-assets/build/css/custom.min.css') }}" rel="stylesheet">
+    {{--  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Modern admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
+    <meta name="keywords" content="admin template, modern admin template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">  --}}
+    <meta name="author" content="kallbuloso">
+    <title>Register with Background Color - Modern Admin - Clean Bootstrap 4 Dashboard HTML Template + Bitcoin Dashboard</title>
+    <link rel="apple-touch-icon" href="ma/app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="ma/app-assets/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Quicksand:300,400,500,700" rel="stylesheet">
+    <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css" rel="stylesheet">
+    <!-- BEGIN VENDOR CSS-->
+    <link rel="stylesheet" type="text/css" href="ma/app-assets/css/vendors.css">
+    <link rel="stylesheet" type="text/css" href="ma/app-assets/vendors/css/forms/icheck/icheck.css">
+    <link rel="stylesheet" type="text/css" href="ma/app-assets/vendors/css/forms/icheck/custom.css">
+    <!-- END VENDOR CSS-->
+    <!-- BEGIN MODERN CSS-->
+    <link rel="stylesheet" type="text/css" href="ma/app-assets/css/app.css">
+    <!-- END MODERN CSS-->
+    <!-- BEGIN Page Level CSS-->
+    <link rel="stylesheet" type="text/css" href="ma/app-assets/css/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="ma/app-assets/css/core/colors/palette-gradient.css">
+    <link rel="stylesheet" type="text/css" href="ma/app-assets/css/pages/login-register.css">
+    <!-- END Page Level CSS-->
+    <!-- BEGIN Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="ma/assets/css/style.css">
+    <!-- END Custom CSS-->
   </head>
-
-  <body class="login">
-    <div>
-      <!--a class="hiddenanchor" id="signup"></a>
-      <a class="hiddenanchor" id="signin"></a-->
-
-      <div class="login_wrapper">
-        <div class="animate form login_form">
-          <section class="login_content">
-            @yield('content')
+  <body class="vertical-layout vertical-menu 1-column  bg-full-screen-image menu-expanded blank-page blank-page" data-open="click" data-menu="vertical-menu" data-col="1-column">
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+    <div class="app-content content">
+      <div class="content-wrapper">
+        <div class="content-header row">
+        </div>
+        <div class="content-body">
+          <section class="flexbox-container">
+            <div class="col-12 d-flex align-items-center justify-content-center">
+              <div class="col-md-4 col-10 box-shadow-2 p-0">
+                <div class="card border-grey border-lighten-3 px-1 py-1 m-0">
+                @yield('content')
+                </div>
+              </div>
+            </div>
           </section>
         </div>
       </div>
     </div>
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+  <!-- BEGIN VENDOR JS-->
+  <script src="ma/app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
+  <!-- BEGIN VENDOR JS-->
+  <!-- BEGIN PAGE VENDOR JS-->
+  <script src="ma/app-assets/vendors/js/forms/validation/jqBootstrapValidation.js"
+  type="text/javascript"></script>
+  <script src="ma/app-assets/vendors/js/forms/icheck/icheck.min.js" type="text/javascript"></script>
+  <!-- END PAGE VENDOR JS-->
+  <!-- BEGIN MODERN JS-->
+  <script src="ma/app-assets/js/core/app-menu.js" type="text/javascript"></script>
+  <script src="ma/app-assets/js/core/app.js" type="text/javascript"></script>
+  <!-- END MODERN JS-->
+  <!-- BEGIN PAGE LEVEL JS-->
+  <script src="ma/app-assets/js/scripts/forms/form-login-register.js" type="text/javascript"></script>
+  <!-- END PAGE LEVEL JS-->  
   </body>
 </html>
